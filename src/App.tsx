d99bb0ed819af1bfb70801d7f9e8e5021e808692
nikskr <91470938+nikskr@users.noreply.github.com>
2026-06-26 16:48:11 +0300

    update: add currencies table, fix input

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,31 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [rates, setRates] = useState({});
-  const [currencyList, setCurrencyList] = useState([]);
+  const [rates, setRates] = useState<Record<string, number>>({"BYN": 3, "RUB": 100, "USD": 1});
+  const [currencyList, setCurrencyList] = useState<string[]>([]);
   const [fromCurrency, setFromCurrency] = useState('USD');
   const [toCurrency, setToCurrency] = useState('BYN');
-  const [amount, setAmount] = useState(1);
+  const [amount, setAmount] = useState<string>('1');
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+  const [focusedInput, setFocusedInput] = useState<'from' | 'to' | null>(null);
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const targetRate = rates[toCurrency] || 0;
+  const numericAmount = Number(amount);
   let fromAmount = 0;
   let toAmount = 0;
 
-  (function calculateCurrency() {
-    const targetRate = rates[toCurrency] || 0;
-
-      if (targetRate > 0) {
-        if (amountInFromCurrency) {
-          fromAmount = amount;
-          toAmount = amount * targetRate;
-        } else {
-          toAmount = amount;
-          fromAmount = amount / targetRate;
-        }
-      }
-  })();
+  if (targetRate > 0) {
+    if (amountInFromCurrency) {
+      fromAmount = Number(numericAmount.toFixed(2));
+      toAmount = Number((numericAmount * targetRate).toFixed(2));
+    } else {
+      toAmount = Number(numericAmount.toFixed(2));
+      fromAmount = Number((numericAmount / targetRate).toFixed(2));
+    }
+  }
 
   useEffect(() => {
     const API_KEY = import.meta.env.VITE_EXCHANGE_API_KEY;
@@ -49,6 +48,7 @@ function App() {
         }
       })
       .catch(e => {
+        setCurrencyList(Object.keys({"BYN": 3, "RUB": 100, "USD": 1}));
         setError(e);
         console.log(e);
       })
@@ -76,7 +76,7 @@ function App() {
       <h1>Currency Converter</h1>
       <div className='converter-box'>
         <div className='converter-box__calculator'>
-          <input type="number" value={fromAmount ? Number(fromAmount).toFixed(2) : ''} onChange={handleFromAmountChange} placeholder='From...' min={0}/>
+          <input type="number" value={focusedInput === 'from' ? fromAmount : Number(fromAmount).toFixed(2)} onChange={handleFromAmountChange} placeholder='From...' min={0} onFocus={() => setFocusedInput('from')} onBlur={() => setFocusedInput(null)}/>
           <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
             {currencyList.map((code) => 
               <option value={code} key={code}>{code}</option>
@@ -85,10 +85,12 @@ function App() {
 
           <strong> = </strong>
 
-          <input type="number" value={toAmount ? Number(toAmount).toFixed(2) : ''} onChange={handleToAmountChange} placeholder='To...' min={0}/>
+          <input type="number" value={focusedInput === 'to' ? toAmount : Number(toAmount).toFixed(2)} onChange={handleToAmountChange} placeholder='To...' min={0} onFocus={() => setFocusedInput('to')} onBlur={() => setFocusedInput(null)}/>
           <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
-            {currencyList.map((code) => 
-              <option value={code} key={code}>{code}</option>
+            {currencyList.map((code) => {
+              console.log(code);
+              return <option value={code} key={code}>{code}</option>
+            }
             )}
           </select>
         </div>
@@ -98,6 +100,19 @@ function App() {
             Amount in from currency
           </label>
         </div>
+      </div>
+      <div className='currency-info'>
+            <h2 className='currency-info__header'>1 {fromCurrency}</h2>
+            <hr style={{width: "100%"}}/>
+            <div className='currency-info__container'>
+              {Object.entries(rates).map(([key, value]) => {
+                return (
+                  <div className='currency-info__item' key={key}>
+                    {value + ' ' + key}
+                  </div>
+                )
+              })}
+            </div>
       </div>
     </div>
   )
--- src/components/Input/AmountInput.tsx
+++ src/components/Input/AmountInput.tsx
@@ -1,17 +1,17 @@
 
-function AmountInput({amountInFromCurrency, amount, handleFromAmountChange, rates, fromCurrency}) {
+// function AmountInput({amountInFromCurrency, amount, handleFromAmountChange, rates, fromCurrency}) {
 
-  function calculateCurrency(): Number {
-      if (rates[fromCurrency] && rates[toCurrency]) {
-        return amountInFromCurrency ? (amount * rates[toCurrency] / rates[fromCurrency]) : (amount * rates[fromCurrency] / rates[toCurrency]);
-      } else {
-        return 0;
-      }
-    }
+//   function calculateCurrency(): Number {
+//       if (rates[fromCurrency] && rates[toCurrency]) {
+//         return amountInFromCurrency ? (amount * rates[toCurrency] / rates[fromCurrency]) : (amount * rates[fromCurrency] / rates[toCurrency]);
+//       } else {
+//         return 0;
+//       }
+//     }
       
-  return (
-      <input type="number" value={String(amountInFromCurrency ? amount : calculateCurrency())} onChange={handleFromAmountChange} placeholder='From...' min={0}/>
-  )
-}
+//   return (
+//       <input type="number" value={String(amountInFromCurrency ? amount : calculateCurrency())} onChange={handleFromAmountChange} placeholder='From...' min={0}/>
+//   )
+// }
 
-export default AmountInput;
+// export default AmountInput;
